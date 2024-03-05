6515204b5b1c85d6c95d7afee54a46e27993319a
AnkiiB <149452881+AnkiiB@users.noreply.github.com>
2024-03-05 13:57:21 +0530

    Add main file

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,6 @@ int main(void) {
   cout << "Hello, Ankith\n";
   cout << "Hello, Jenkins (after change)\n";
   cout << "I have successfully built and run\n";
-    cout << "Webhook successfully built and run\n";
+  cout << "Webhook successfully built and run\n";
 
   return 0;
-}i
